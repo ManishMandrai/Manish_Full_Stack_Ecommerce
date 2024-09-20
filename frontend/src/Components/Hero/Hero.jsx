1f7099bedd7d2bdd0df4cb27e8225687e8b52fd0
ManishMandrai <manishkumarmandrai@gmail.com>
2024-09-20 23:46:37 +0530

    .

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -8,13 +8,13 @@ const Hero = () => {
   return (
     <div className="hero">
       <div className="hero-left">
-        <h2>NEW ARRIVALS ONLY</h2>
+        <h2>Discover the Latest Arrivals</h2>
         <div>
           <div className="hero-hand-icon">
-            <p>new</p>
+            <p>Trending</p>
             <img src={hand_icon} alt="" />
           </div>
-          <p>collections</p>
+          <p>Selections</p>
           <p>for everyone</p>
         </div>
         <div className="hero-latest-btn">
